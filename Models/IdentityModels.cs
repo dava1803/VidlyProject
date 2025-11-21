528e982c52a86e9db920cc037012fb85428d74e9
dava1803 <ptdv0551@gmail.com>
2025-11-22 00:06:14 +0700

    setup OWIN & Identity; Tambah AccountController untuk fitur login/logout & memperbaiki authentication

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -6,31 +6,33 @@ using System.Data.Entity;
 
 namespace VidlyProject.Models
 {
-    // Custom user class
     public class ApplicationUser : IdentityUser
     {
-        // Tambahan properti custom
-        // public string FullName { get; set; }
-
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Tambahkan claim custom jika perlu
-            // userIdentity.AddClaim(new Claim("FullName", this.FullName ?? ""));
+            var userIdentity = await manager.CreateIdentityAsync(
+                this,
+                DefaultAuthenticationTypes.ApplicationCookie);
+
             return userIdentity;
         }
     }
 
-    // DbContext untuk Identity + model aplikasi
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext() : base("DefaultConnection") { }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
 
         public DbSet<Customer> Customers { get; set; }
-        //public DbSet<MembershipType> MembershipTypes { get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<MembershipType> MembershipTypes { get; set; }
-        public DbSet<Genre> Genres { get; set; }  
+        public DbSet<Genre> Genres { get; set; }
     }
-
 }
