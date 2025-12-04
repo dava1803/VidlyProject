b2d6a2685cedc3ddb7f09f59e6edf7c021e80324
dava1803 <ptdv0551@gmail.com>
2025-12-04 11:10:35 +0700

    memisah class ApplicationDbContext dan ApplicationUser

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using System.Data.Entity;
 using System.ComponentModel.DataAnnotations;
 
 namespace VidlyProject.Models
@@ -16,6 +15,7 @@ namespace VidlyProject.Models
         [Required]
         [StringLength(255)]
         public string DrivingLicense { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             var userIdentity = await manager.CreateIdentityAsync(
@@ -24,23 +24,5 @@ namespace VidlyProject.Models
 
             return userIdentity;
         }
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<MembershipType> MembershipTypes { get; set; }
-        public DbSet<Genre> Genres { get; set; }
     }
 }
